69f9dd3780e5388845a98809194e9693c4c52d46
McClelland Kemp <McClelland.Kemp@iriworldwide.com>
2017-05-26 08:15:50 -0500

    adding schedule for the 2017 Summer session of EGR

--- data-raw/raw_data_create.R
+++ data-raw/raw_data_create.R
@@ -1,6 +1,8 @@
 schedules <- list(
   `deployedcourses/uiuc_netmath_math461_r2012_mm` = data.table::fread("data-raw/netmath461.csv")
   , `deployedcourses/uiuc_netmath_ma461egr_fs2016_fall16` = data.table::fread("data-raw/netmath461EGR.csv")
+  , `deployedcourses/uiuc_netmath_math461_sps2017_egr` = data.table::fread("data-raw/netmath461EGR.csv")
+  , `deployedcourses/uiuc_netmath_ma461_sus2017_egr` = data.table::fread("data-raw/netmath461EGRSummer2017.csv")
 )
 
 status <- data.table::data.table(
