a9bc5c570e6cd347cce90411d84d6d6770b08a02
McClelland Kemp <McClelland.Kemp@iriworldwide.com>
2017-09-27 09:46:50 -0500

    updating schedules with FA2017 461EGR schedule

--- data-raw/raw_data_create.R
+++ data-raw/raw_data_create.R
@@ -2,6 +2,7 @@ schedules <- list(
   `deployedcourses/uiuc_netmath_math461_r2012_mm` = data.table::fread("data-raw/netmath461.csv")
   , `deployedcourses/uiuc_netmath_ma461egr_fs2016_fall16` = data.table::fread("data-raw/netmath461EGR.csv")
   , `deployedcourses/uiuc_netmath_math461_sps2017_egr` = data.table::fread("data-raw/netmath461EGR.csv")
+  , `deployedcourses/uiuc_netmath_math461_fs2017_egr` = data.table::fread("data-raw/netmath461EGR.csv")
   , `deployedcourses/uiuc_netmath_ma461_sus2017_egr` = data.table::fread("data-raw/netmath461EGRSummer2017.csv")
 )
 
